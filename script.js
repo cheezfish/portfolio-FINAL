d98bcc1a8c76bc247c110c25d4d91d86ca530722
cheezfish <imran-aziz@mail.com>
2021-09-23 12:17:37 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.boxbox').slick({
+        lazyLoad: 'ondemand',
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: false,
+        pauseOnHover: true,
+    });
+  });
